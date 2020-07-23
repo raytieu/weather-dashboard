fdb2135bd886a87dad54e8f7d82f0762937efe0f
Ray Tieu <ray.tieu@gmail.com>
2020-07-22 18:04:35 -0700

    added comments to files

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
 
+  // API Key to retrieve API call from openweather.org
   let apiKey = "69de6a59efcfe98cdc7b78dff1d7d1b2";
+
+  // Variables to query HTML elements
   let searchBtn = $("#button-addon2");
   let currentWeather = $("#current-weather");
   let forecastWeather = $(".five-day");
 
+  // Array of cities in the search history that is retrieved from Local Storage
   let storedCities = JSON.parse(localStorage.getItem("cities")) || [];
 
+  // Render Search History and the most recent result upon loading page
   searchHistory();
   lastResult();
   
+  // Functions to display current and future weather information and to push new search into storedCities array
   $(".search-card").click(displayWeather);
   searchBtn.click(newSearch);
   $(".form-control").keypress(function(event) {
@@ -18,14 +24,21 @@ $(document).ready(function() {
     }
   });
 
+
+  // Function when entering a new city search
   function newSearch() {
 
+    // Clear current and future weather sections
     currentWeather.empty();
     forecastWeather.empty();
 
+    // Retrieve city name from input field to query in the URL
     let city = $("#city-name").val();
+
+    // Endpoint to call the Current Weather API 
     let queryCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     
+    // AJAX call to populate the Current Weather section
     $.ajax({
       url: queryCurrent,
       method: "GET"
@@ -37,19 +50,25 @@ $(document).ready(function() {
       let humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
       let windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
 
+      // Latitude and Longitude coordinates needed for the One Call API Endpoint
       let lat = response.coord.lat;
       let lon = response.coord.lon;
+
+      // Endpoint to call the One Call API (UV Index in Current Weather, and all of 5-day Forecast)
       let queryForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}`
 
+      // AJAX call for UV Index and 5-day Forecast
       $.ajax({
         url: queryForecast,
         method: "GET"
       }).then(function(response) {
         console.log(response);
 
+        // Retrieve UV Index and append to Current Weather Section
         let uvIndex = $("<p>").text("UV Index: ").append($("<span>").addClass("uv-index").text(response.current.uvi));
         currentWeather.append(uvIndex);
 
+        // Conditions for background color of UV Index value
         if (response.current.uvi < 3) {
           $(".uv-index").css("background-color", "green");
         }
@@ -60,8 +79,10 @@ $(document).ready(function() {
           $(".uv-index").css("background-color", "red");
         }
 
+        // Append "5-day Forecast" text
         $(".five-day").append($("<p>").css("font-size", "25px").css("font-weight", "bold").text("5-Day Forecast:")).append($("<div>").addClass("row forecast-area"));
 
+        // For loop to create each individual 5-day forecast card
         for (let i = 1; i < 6; i++) {
           let forecastArea = $(".forecast-area");
           let newForecast = $("<div>").addClass("card col-xs-12 col-s text-white bg-primary mb-3 forecast-card");
@@ -78,33 +99,44 @@ $(document).ready(function() {
 
       });
 
+      // Append selected information into the Current Weather section
       currentWeather.append(cityName).append(tempF).append(humidity).append(windSpeed);
       
+      // Push searched city into local storage if city does not exist in storage previously
       if (storedCities.indexOf(response.name) === -1) {
         storedCities.push(response.name);
         localStorage.setItem("cities", JSON.stringify(storedCities));
       }
       
+      // Empty old search history and render updated one from LocalStorage
       $("#search-history").empty();
       searchHistory();
 
+      // Render displayWeather function when clicking on a city in the history section
       $(".search-card").click(displayWeather);
 
     });
 
+    // Clear input field after searching for a city
     $(".form-control").val('');
     
   } 
 
 
+  // Function to display weather sections when clicking on a city in the search history
   function displayWeather() {
 
+    // Clear current and future weather sections
     currentWeather.empty();
     forecastWeather.empty();
 
+    // Retrieve city name from data value of city to query in the URL
     let city = $(this).attr("data-name");
+
+    // Endpoint to call the Current Weather API 
     let queryCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
    
+    // AJAX call to populate the Current Weather section
     $.ajax({
       url: queryCurrent,
       method: "GET"
@@ -115,18 +147,24 @@ $(document).ready(function() {
       let humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
       let windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
 
+      // Latitude and Longitude coordinates needed for the One Call API Endpoint
       let lat = response.coord.lat;
       let lon = response.coord.lon;
+
+      // Endpoint to call the One Call API (UV Index in Current Weather, and all of 5-day Forecast)
       let queryForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}`
 
+      // AJAX call for UV Index and 5-day Forecast
       $.ajax({
         url: queryForecast,
         method: "GET"
       }).then(function(response) {
 
+        // Retrieve UV Index and append to Current Weather Section
         let uvIndex = $("<p>").text("UV Index: ").append($("<span>").addClass("uv-index").text(response.current.uvi));
         currentWeather.append(uvIndex);
 
+        // Conditions for background color of UV Index value
         if (response.current.uvi < 3) {
           $(".uv-index").css("background-color", "green");
         }
@@ -137,8 +175,10 @@ $(document).ready(function() {
           $(".uv-index").css("background-color", "red");
         }
 
+        // Append "5-day Forecast" text
         $(".five-day").append($("<p>").css("font-size", "25px").css("font-weight", "bold").text("5-Day Forecast:")).append($("<div>").addClass("row forecast-area"));
 
+        // For loop to create each individual 5-day forecast card
         for (let i = 1; i < 6; i++) {
           let forecastArea = $(".forecast-area");
           let newForecast = $("<div>").addClass("card col-xs-12 col-s text-white bg-primary mb-3 forecast-card");
@@ -155,6 +195,7 @@ $(document).ready(function() {
 
       });
 
+      // Append selected information into the Current Weather section
       currentWeather.append(cityName).append(tempF).append(humidity).append(windSpeed);
 
     });
@@ -162,14 +203,20 @@ $(document).ready(function() {
   }
 
 
+  // Function to display weather sections of last city searched when the page loads
   function lastResult() {
 
+    // Clear current and future weather sections
     currentWeather.empty();
     forecastWeather.empty();
 
+    // Retrieve city name of the last city in the storedCities array
     let city = storedCities.slice(-1)[0];
+
+    // Endpoint to call the Current Weather API 
     let queryCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
    
+    // AJAX call to populate the Current Weather section
     $.ajax({
       url: queryCurrent,
       method: "GET"
@@ -180,18 +227,24 @@ $(document).ready(function() {
       let humidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
       let windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
 
+      // Latitude and Longitude coordinates needed for the One Call API Endpoint
       let lat = response.coord.lat;
       let lon = response.coord.lon;
+
+      // Endpoint to call the One Call API (UV Index in Current Weather, and all of 5-day Forecast)
       let queryForecast = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}`
 
+      // AJAX call for UV Index and 5-day Forecast
       $.ajax({
         url: queryForecast,
         method: "GET"
       }).then(function(response) {
 
+        // Retrieve UV Index and append to Current Weather Section
         let uvIndex = $("<p>").text("UV Index: ").append($("<span>").addClass("uv-index").text(response.current.uvi));
         currentWeather.append(uvIndex);
 
+        // Conditions for background color of UV Index value
         if (response.current.uvi < 3) {
           $(".uv-index").css("background-color", "green");
         }
@@ -202,8 +255,10 @@ $(document).ready(function() {
           $(".uv-index").css("background-color", "red");
         }
 
+        // Append "5-day Forecast" text
         $(".five-day").append($("<p>").css("font-size", "25px").css("font-weight", "bold").text("5-Day Forecast:")).append($("<div>").addClass("row forecast-area"));
 
+        // For loop to create each individual 5-day forecast card
         for (let i = 1; i < 6; i++) {
           let forecastArea = $(".forecast-area");
           let newForecast = $("<div>").addClass("card col-xs-12 col-s text-white bg-primary mb-3 forecast-card");
@@ -220,6 +275,7 @@ $(document).ready(function() {
 
       });
 
+      // Append selected information into the Current Weather section
       currentWeather.append(cityName).append(tempF).append(humidity).append(windSpeed);
 
     });
@@ -227,6 +283,7 @@ $(document).ready(function() {
   }
 
 
+  // Function to create a card for each city in the storedCities array that can be clicked on to display weather info
   function searchHistory() {
     for (let i = 0; i < storedCities.length; i++) {
       let cityPrepend = $("<div>").addClass("card search-card").attr("data-name", storedCities[i]).text(storedCities[i]);
@@ -235,6 +292,7 @@ $(document).ready(function() {
   }
 
 
+  // Click function to clear all sections of displayed information, and also clear the storedCities array and Local Storage
   $("#clear").click(function() {
     localStorage.removeItem("cities");
     currentWeather.empty();
